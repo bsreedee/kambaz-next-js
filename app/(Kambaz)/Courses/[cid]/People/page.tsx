c0134c75b2782456fdeceb19cff6d389e0ead232
Sree Dee <bsreedee@gmail.com>
2025-12-01 00:12:18 -0500

    Done with People Courses

--- app/(Kambaz)/Courses/[cid]/People/page.tsx
+++ app/(Kambaz)/Courses/[cid]/People/page.tsx
@@ -1,10 +1,29 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useParams } from "next/navigation";
 import PeopleTable from "./Table/page";
+import * as client from "../../client";
 
 export default function People() {
+  const { cid } = useParams();
+  const [users, setUsers] = useState([]);
+
+  const fetchUsers = async () => {
+    if (!cid) return;
+    const users = await client.findUsersForCourse(cid as string);
+    setUsers(users);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, [cid]);
+
   return (
     <div>
       <h2>People</h2>
       <hr />
-      <PeopleTable users={[]} fetchUsers={() => {}}/>
+      <PeopleTable users={users} fetchUsers={fetchUsers} />
     </div>
-);}
+  );
+}
